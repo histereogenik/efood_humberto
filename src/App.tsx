9bbc98a6f8d5f88ee7a428d2ae9edd0dc96b767a
Humberto Nacif <gigantedeluz@gmail.com>
2024-01-08 21:07:47 -0300

    Configuring RTK Query for the Restaurants api

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import RoutesComponent from './routes'
 import Footer from './components/Footer'
 
 import { GlobalCss } from './styles'
+import { store } from './store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalCss />
-      <RoutesComponent />
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalCss />
+        <RoutesComponent />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
